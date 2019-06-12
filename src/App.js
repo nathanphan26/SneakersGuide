c1eefbdb23f65037892470160f6ea44d91fe82bb
Nathan Phan <nathphan510@gmail.com>
2019-06-11 21:04:01 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+ 
+import Header from './components/Header'
 
 function App() {
+
+  const testStyle = {
+    // backgroundColor: 'red',
+    height: '100%',
+    paddingTop: '20px'
+    // marginLeft: '10%',
+    // marginRight:'10%'
+  }
+
+  const headerStyle = {
+
+  }
+
+  const rowStyle = {
+    height: '100%'
+  }
+
+  const colStyle1 = {
+    height: '100%',
+    backgroundColor: 'red',
+    textAlign: 'center'
+  }
+
+  const colStyle2 = {
+    height: '100%',
+    backgroundColor: 'blue',
+    textAlign: 'center'
+  }
+
+  const colStyle3 = {
+    height: '100%',
+    backgroundColor: 'green',
+    textAlign: 'center'
+  }
+
+  const innerRowStyle = {
+    height: '300px'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header style={headerStyle}/>
+      <div style={testStyle}>
+        <Row style={rowStyle}>
+          <Col xs='2' style={colStyle1}>Hello</Col>
+          <Col xs='8' style={colStyle2}>
+            <Row style={innerRowStyle}>
+              <Col>1</Col>
+              <Col>2</Col>
+            </Row>
+            <Row style={innerRowStyle}>
+              <Col>3</Col>
+              <Col>4</Col>
+            </Row>
+          </Col>
+          <Col xs='2' style={colStyle3}>Hello</Col>
+        </Row>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">SneakerGuide</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/analysis/">Post-Drop-Analysis</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/subscribe/">Subscribe</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+  }
+}
+
+export default Header
