bbe289c570a3182733b05057b7fa32b6600565ec
Nathan Phan <nathphan510@gmail.com>
2019-06-12 21:33:49 -0700

    Media Query

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import Placeholder from '../assets/placeholder.jpg'
+import { Row, Col } from 'reactstrap';
+import MediaQuery from 'react-responsive';
+
 import YeezyBlack from '../assets/yeezy_black.jpg'
 import YeezySynth from '../assets/yeezy_synth.jpg'
 import YeezyLundmark from '../assets/yeezy_lundmark.jpg'
-import { Row, Col } from 'reactstrap';
 
 import Image from './Image';
 
@@ -23,33 +24,77 @@ export class Main extends Component {
       overflow: 'auto'
     }
 
-    const innerColStyle = {
-      padding: '0'
-    }
-
-    const divStyle = {
-      background: `url('${YeezyBlack}')`,
-      height: '350px',
-      width: '350px',
-      paddingTop: '25px',
-      margin: '25px',
-      verticalAlign: 'middle'
-    }
-
     return (
       <Col xs='8' style={colStyle2}>
-        <Row style={innerRowStyle}>
-          <Image src={YeezyBlack}/>
-          <Image src={YeezyBlack}/>
-        </Row>
-        <Row style={innerRowStyle}>
-          <Image src={YeezyLundmark}/>
-          <Image src={YeezyLundmark}/>
-        </Row>
-        <Row style={innerRowStyle}>
-          <Image src={YeezySynth}/>
-          <Image src={YeezySynth}/>
-        </Row>
+        <MediaQuery minDeviceWidth={1200}>
+          {(matches) => {
+            if (matches) {
+              return (
+                <Row style={innerRowStyle}>
+                  <Image src={YeezyBlack}/>
+                  <Image src={YeezyBlack}/>
+                </Row>
+              );
+            } else {
+              return (
+                <React.Fragment>
+                  <Row style={innerRowStyle}>
+                    <Image src={YeezyBlack}/>
+                  </Row>
+                  <Row style={innerRowStyle}>
+                    <Image src={YeezyBlack}/>
+                  </Row>
+                </React.Fragment>
+              );
+            }
+          }}
+        </MediaQuery>
+        <MediaQuery minDeviceWidth={1200}>
+          {(matches) => {
+            if (matches) {
+              return (
+                <Row style={innerRowStyle}>
+                  <Image src={YeezyBlack}/>
+                  <Image src={YeezyBlack}/>
+                </Row>
+              );
+            } else {
+              return (
+                <React.Fragment>
+                  <Row style={innerRowStyle}>
+                    <Image src={YeezyBlack}/>
+                  </Row>
+                  <Row style={innerRowStyle}>
+                    <Image src={YeezyBlack}/>
+                  </Row>
+                </React.Fragment>
+              );
+            }
+          }}
+        </MediaQuery>
+        <MediaQuery minDeviceWidth={1200}>
+          {(matches) => {
+            if (matches) {
+              return (
+                <Row style={innerRowStyle}>
+                  <Image src={YeezyBlack}/>
+                  <Image src={YeezyBlack}/>
+                </Row>
+              );
+            } else {
+              return (
+                <React.Fragment>
+                  <Row style={innerRowStyle}>
+                    <Image src={YeezyBlack}/>
+                  </Row>
+                  <Row style={innerRowStyle}>
+                    <Image src={YeezyBlack}/>
+                  </Row>
+                </React.Fragment>
+              );
+            }
+          }}
+        </MediaQuery>
       </Col>
     )
   }
